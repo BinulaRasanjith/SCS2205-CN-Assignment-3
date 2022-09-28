82959658906a2c73826daa8b744ef9db1b95efa9
BinulaRasanjith <binula28@gmail.com>
2022-09-28 12:39:58 +0530

    CN assignment 3 uploaded

---
+++ Readme.md
@@ -0,0 +1,30 @@
+-- INSTRUCTIONS --
+
+Step 1: Download & install python3 to your computer 
+        - To download python3 use below link
+        - (Download a compatible version for your operating system)
+        - Link: https://www.python.org/downloads/
+
+Step 2: Open the terminal in the directory containing the "server.py" file
+        - Go to the folder containing the "server.py" file
+        - Click on File Explorer Address bar
+        - Clear selected address
+        - Type "cmd" in the address bar & press Enter
+        - (Terminal will open in the current directory)
+
+Step 3: Start the simple server
+        - Type "python server.py" in the terminal
+        - Server will start and display this message in the Terminal
+            Serever socket created successfully
+            Socket is binded to Port: 2728
+            Socket is listning...
+
+Step 4: Open the browser
+        - Type http://localhost:2728 in the address bar
+        - Webpage will be open in the browser
+
+DONE!!!
+
+****************************************************************
+To stop the server,
+Press Ctrl+C in the terminal and refresh the browser.
---
+++ server.py
@@ -0,0 +1,77 @@
+from re import L
+from socket import socket, AddressFamily, SocketKind
+from os import path, getcwd
+
+LOCALHOST = '127.0.0.1'
+PORT = 2728
+
+
+def get_resource_path(http_path):
+    # if http_path endswith '/' remove it
+    if http_path.endswith('/'):
+        http_path = http_path[:-1]
+    print(http_path)
+    # spliting http_path from '.'
+    path_seg = http_path.split('.')
+    if http_path != '':  # if http_path is not a empty string
+        if len(path_seg) > 1:  # if there are '.' there could be an extension in the path
+            # therefore resource path is relevent resource from the htdocs folder
+            resource_path = path.join(getcwd(), 'htdocs', http_path[1:])
+        else:  # if there is no '.' in the path add '.html' to the http path
+            resource_path = path.join(getcwd(), 'htdocs', f'{http_path[1:]}.html')
+    else:  # if the http_path is a empty string give the index.html as the resource path
+        resource_path = path.join(getcwd(), 'htdocs', 'index.html')
+    print(resource_path, '\n-----------------------------')
+    return resource_path
+
+
+# AF_INET -> ipv4, SOCK_STREAM -> TCP/IP protocol
+server_socket = socket(AddressFamily.AF_INET, SocketKind.SOCK_STREAM)
+print('Serever socket created successfully')
+
+# bind ip & port into the socket
+server_socket.bind((LOCALHOST, PORT))
+print(f'Socket is binded to Port: {PORT}')
+
+# start listning in the socket
+server_socket.listen()
+print('Socket is listning...')
+
+# starting a infinite loop for server to keep responding to the client
+while True:
+    # accepting the connection with the client
+    client_socket, client_address = server_socket.accept()
+    print("Got connected from ", client_address)
+
+    # getting the request from the client
+    client_request = client_socket.recv(1024).decode()
+
+    # splitting http headers from the request and store it in a array
+    http_headers = client_request.split('\r\n')
+
+    # getting 1st header of the client request and split it from spaces
+    http_header_0 = http_headers[0].split(' ')
+
+    try:  # using try-except to catch the errors
+        # getting the http url from the 1st header
+        http_url = http_header_0[1]
+        # removing any queries from the url
+        http_path = http_url.split('?')[0]
+        # getting the resource path according to the http path
+        resource_path = get_resource_path(http_path)
+
+        try:  # using try-except to catch the errors
+            # opening the relevent resource
+            resource = open(resource_path, 'r')
+            # reading the resource content and store it ina variable
+            resource_content = resource.read()
+            resource.close()
+            # storing the response from the resource content
+            http_response = f'HTTP/1.1 200 OK\r\n\n{resource_content}'
+            # sending the response encoded to the client
+            client_socket.sendall(http_response.encode())
+        except FileNotFoundError:  # if the resource not found, show file not found
+            client_socket.sendall('HTTP/1.1 404 Not Found'.encode())
+    except IndexError:  # ifthere is an index error, show bad request
+        client_socket.sendall("HTTP/1.1 400 Bad Request".encode())
+    client_socket.close()
